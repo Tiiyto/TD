fcc44dfb4d8d23591478187c473eec8dff5d1338
Antonin <lavie.antonin@hotmail.fr>
2020-09-22 15:38:49 +0200

    TD2

--- HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -4,18 +4,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.Switch
 import android.widget.Toast
 
-const val TAG: String = "hello"
+const val TAG: String = "MainActivity"
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val btnClick = findViewById<Button>(R.id.button)
         btnClick?.setOnClickListener(){
             Toast.makeText(this@MainActivity, R.string.message,Toast.LENGTH_SHORT).show()
         }
+        val switch = findViewById<Switch>(R.id.switch2)
+        switch?.setOnCheckedChangeListener({ _ , isChecked ->
+            val message = if (isChecked) "Switch1:ON" else "Switch2:OFF"
+            Toast.makeText(this@MainActivity,message,Toast.LENGTH_SHORT).show()
+        })
         val v = 54
         val n = v/9 - 2
         Log.i(TAG, "n = $n")
